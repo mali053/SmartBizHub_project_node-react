f739910aaebdd514a050f1a5952c0b25d8f2e350
mali053 <m9743740@gmail.com>
2024-06-17 22:03:15 +0300

    change the validation to the email on the model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { isEmail } = require('validator');
 
 const Schema = mongoose.Schema
 
@@ -6,9 +7,19 @@ const UserSchema = new Schema({
   username: { type: String, required: true, maxLength: 100 },
   password: { type: String, required: true, minLength: 5 },
   phone: { type: String, required: true },
-  email: { type: String },
+  email: { 
+    type: String,
+    validate: {
+      validator: function(v) {
+        return isEmail(v); // בדיקת תקינות האימייל באמצעות ה-validator של Mongoose
+      },
+      message: props => `${props.value} is not a valid email address!`
+    },
+    unique: true // אימות שהאימייל הוא ייחודי במסד הנתונים
+  },
   userType: { type: String, required: true, enum: ['admin', 'client'] } // סוג משתמש: מנהל או לקוח
-})
+});
+
 
 UserSchema.virtual('url').get(function () {
   return `/user/${this._id}`
