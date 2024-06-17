a702557dc2fd61c41aa2a19789e24924a1ac744f
mali053 <m9743740@gmail.com>
2024-06-17 11:42:12 +0300

    create middleware to verified if thu user have a token

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
+exports.loggedIn = function (req, res, next) {
+  let token = req.header('Authorization')
+  if (!token) return res.status(401).send('Access Denied')
+
+  try {
+    if (token.startsWith('Bearer ')) {
+      // Remove Bearer from string
+      token = token.slice(7, token.length).trimLeft()
+    }
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+    req.user = verified
+    next()
+  } catch (err) {
+    res.status(400).send('Invalid Token')
+  }
+}
+
+exports.adminOnly = async function (req, res, next) {
+  if (req.user.user_type_id === parseInt(process.env.ADMIN_USER_TYPE_ID, 10)) {
+    return res.status(401).send('Access Denied')
+  }
+  next()
+}
